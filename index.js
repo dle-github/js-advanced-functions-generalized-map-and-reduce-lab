618d602341ccb11df6b1d8c831d044442576f0ca
derek <derekle.creative@gmail.com>
2021-08-14 19:14:17 -0500

    commit tree b2f2b1e6a891f5fde2a7d476d2697a57f7dc78b9

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+function map(arr, f){
+    return arr.map(f);
+}
+
+function reduce(arr, f, int){
+    if (!!int) {
+        return arr.reduce(f, int)
+    } else {
+        return arr.reduce(f)
+    }
+}
